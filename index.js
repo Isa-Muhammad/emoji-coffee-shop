9778341e9c798bd698f9ea86ef4c229ad5245f28
Isa-Muhammad <isamuhammadm.im@gmail.com>
2025-03-20 11:29:26 +0100

    Handle routing and return json data

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const fs = require("fs");
 const http = require("http");
 
+const data = fs.readFileSync("./data/data.json", "utf-8");
+
+// console.log("DATA:", data);
+
 const server = http.createServer((req, res) => {
-  res.end("Welcome to emoji coffee shop!");
+  const path = req.url;
+  if (path === "/" || path === "/home") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("Not found!");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
